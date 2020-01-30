13df12bf34e379cd61a5806a05ecab587613cf8f
Stephen Axtell <axtell.stephen@gmail.com>
2020-01-30 15:51:03 +0200

    started working on the hero section

--- js/main.js
+++ js/main.js
@@ -16,6 +16,17 @@ $(document).ready(function() {
 		} else {
 			$('.nav').removeClass('sticky');
 		}
-	})
+	});
+
+	// For bxslider
+	$('.bxslider').bxSlider({
+		mode: 'horizontal',
+		moveSlides: 1,
+		slideMargin: 40,
+		infiniteLoop: true,
+		minSlides: 1,
+		maxSlides: 1,
+		speed: 1200
+	});	
 
 });
